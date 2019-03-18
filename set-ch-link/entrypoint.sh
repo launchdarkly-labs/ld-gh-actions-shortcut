3d1d722073ee9a01b97e1c185361fade19e3009a
Andrew S. Brown <andrew@launchdarkly.com>
2019-03-18 07:55:15 -0700

    Always attach the clubhouse url even if there isn't a placeholder for it
    Also get the ticket # from the PR title if it's available and give it
    precedence over the branch name so the user has maximum flexibility.

--- set-ch-link/entrypoint.sh
+++ set-ch-link/entrypoint.sh
@@ -10,10 +10,23 @@ body=${body//$'\r'/} # Remove /r, which confuses jq in ok.sh
 title=$(jq -r .pull_request.title "$GITHUB_EVENT_PATH")
 title=${title//$'\r'/} # Remove /r, which confuses jq in ok.sh
 
-ticket=$(expr "$GITHUB_REF" : '.*ch\([[:digit:]]*\).*')
+ticket_from_title=$(expr "$title" : '.*ch\([[:digit:]]*\).*')
+ticket_from_branch=$(expr "$GITHUB_REF" : '.*ch\([[:digit:]]*\).*')
+
+# Check title first for the CH ticket
+ticket="$ticket_from_title"
+if [[ -n "$ticket" ]]; then
+    ticket="$ticket_from_branch" # fall back to the CH ticket # from the branch
+fi
+
 link_url="$STORY_BASE_URL/$ticket"
 
 new_body=${body/$STORY_LINK_TEXT/$link_url}
+
+# If we still don't have the url in the body, tack it on the beginning
+if [[ "$new_body" != *"$link_url"* ]]; then
+    new_body="$link_url\n$new_body"
+fi
 
 # Strip out branch name from the PR title
 new_title="${title/$GITHUB_REF/}"
@@ -21,8 +34,8 @@ new_title="${title/$GITHUB_REF/}"
 # Strip out uppercase branch name
 new_title="${new_title/${GITHUB_REF^}/}"
 
-# Add the clubhouse number to the PR title
-if [[ ! "$new_title" =~ "$ticket" ]]; then
+# Add the clubhouse number to the PR title if it isn't already there
+if [[ "$new_title" != *"$ticket"* ]]; then
     new_title="[ch${ticket}] $new_title"
 fi
 

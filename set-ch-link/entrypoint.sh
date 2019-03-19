c61e79b75706e5bee17331b8de0bb7d3c6e43c6a
Andrew Shannon Brown <ashanbrown@users.noreply.github.com>
2019-03-19 15:45:28 -0700

    Always attach the clubhouse url even if there isn't a placeholder for it (#1)
    Also get the ticket # from the PR title if it's available and give it
    precedence over the branch name so the user has maximum flexibility.

--- set-ch-link/entrypoint.sh
+++ set-ch-link/entrypoint.sh
@@ -2,18 +2,35 @@
 
 OK=ok.sh
 
-event=$(cat "$GITHUB_EVENT_PATH")
 number=$(jq -r .number "$GITHUB_EVENT_PATH")
-action=$(jq -r .action "$GITHUB_EVENT_PATH")
 body=$(jq -r .pull_request.body "$GITHUB_EVENT_PATH")
 body=${body//$'\r'/} # Remove /r, which confuses jq in ok.sh
 title=$(jq -r .pull_request.title "$GITHUB_EVENT_PATH")
 title=${title//$'\r'/} # Remove /r, which confuses jq in ok.sh
 
-ticket=$(expr "$GITHUB_REF" : '.*ch\([[:digit:]]*\).*')
+echo "Current PR title is '${title}'"
+
+pattern='.*\bch\([[:digit:]]\+\)\b.*'
+ticket_from_title=$(expr "$title" : "$pattern")
+ticket_from_branch=$(expr "$GITHUB_REF" : "$pattern")
+
+# Check title first for the CH ticket
+ticket="$ticket_from_title"
+if [[ -z "$ticket" ]]; then
+    ticket="$ticket_from_branch" # fall back to the CH ticket # from the branch
+    echo "Found CH ticket number '${ticket}' in branch name"
+else
+    echo "Found CH ticket number '${ticket}' in PR title"
+fi
+
 link_url="$STORY_BASE_URL/$ticket"
 
 new_body=${body/$STORY_LINK_TEXT/$link_url}
+
+# If we still don't have the url in the body, tack it on the beginning
+if [[ "$new_body" != *"$link_url"* ]]; then
+    new_body="$link_url\n$new_body"
+fi
 
 # Strip out branch name from the PR title
 new_title="${title/$GITHUB_REF/}"
@@ -21,8 +38,8 @@ new_title="${title/$GITHUB_REF/}"
 # Strip out uppercase branch name
 new_title="${new_title/${GITHUB_REF^}/}"
 
-# Add the clubhouse number to the PR title
-if [[ ! "$new_title" =~ "$ticket" ]]; then
+# Add the clubhouse number to the PR title if it isn't already there
+if [[ "$new_title" != *"$ticket"* ]]; then
     new_title="[ch${ticket}] $new_title"
 fi
 
@@ -32,6 +49,6 @@ machine api.github.com
     password $GITHUB_TOKEN
 EOF
 
-if [[ "$ticket" != "" ]] && ([[ "$body" != "$new_body" ]] || [[ "$title" != "$new_title" ]]); then
+if [[ "$ticket" != "" && ( "$body" != "$new_body" || "$title" != "$new_title" ) ]]; then
     "$OK" update_pull_request "$GITHUB_REPOSITORY" "$number" "body='$new_body'" "title='$new_title'"
 fi

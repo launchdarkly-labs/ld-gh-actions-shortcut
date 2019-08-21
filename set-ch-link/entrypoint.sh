acd2fb64b2ecbd7376815a2d4f9fc53e90267cbd
Andrew S. Brown <andrew@launchdarkly.com>
2019-08-21 08:19:06 -0700

    Comment when a story is missing

--- set-ch-link/entrypoint.sh
+++ set-ch-link/entrypoint.sh
@@ -13,36 +13,38 @@ echo "Current PR title is '${title}'"
 echo "Github ref is '$branch'"
 
 pattern='.*\bch\([[:digit:]]\+\)\b.*'
-ticket_from_title=$(expr "$title" : "$pattern")
-ticket_from_branch=$(expr "$branch" : "$pattern")
+story_from_title=$(expr "$title" : "$pattern")
+story=$(expr "$branch" : "$pattern")
 
-# Check title first for the CH ticket
-ticket="$ticket_from_title"
-if [[ -z "$ticket" ]]; then
-    ticket="$ticket_from_branch" # fall back to the CH ticket # from the branch
-    echo "Found CH ticket number '${ticket}' in branch name"
+# Check title first for the CH story
+story="$story_from_title"
+if [[ -z "$story" ]]; then
+    story="$story" # fall back to the CH story # from the branch
+    echo "Found CH story number '${story}' in branch name"
 else
-    echo "Found CH ticket number '${ticket}' in PR title"
+    echo "Found CH story number '${story}' in PR title"
 fi
 
-link_url="$STORY_BASE_URL/$ticket"
+link_url="$STORY_BASE_URL/$story"
 
-new_body=${body/$STORY_LINK_TEXT/$link_url}
+if [[ -n "$story" ]]; then
+    new_body=${body/$STORY_LINK_TEXT/$link_url} # replace the story link text
+fi
 
 # If we still don't have the url in the body, tack it on the beginning
-if [[ "$new_body" != *"$link_url"* ]]; then
+if [[ "$new_body" != *"$link_url"* ]] && [[ -n "$story" ]] ; then
     new_body="$link_url\n$new_body"
 fi
 
 # Strip out branch name from the PR title
-new_title="${title/$GITHUB_REF/}"
+new_title="${title/$branch/}"
 
 # Strip out uppercase branch name
-new_title="${new_title/${GITHUB_REF^}/}"
+new_title="${new_title/${branch^}/}"
 
 # Add the clubhouse number to the PR title if it isn't already there
-if [[ "$new_title" != *"$ticket"* ]]; then
-    new_title="[ch${ticket}] $new_title"
+if [[ "$new_title" != *"$story"* ]]; then
+    new_title="[ch${story}] $new_title"
 fi
 
 cat > ~/.netrc <<-EOF
@@ -51,6 +53,12 @@ machine api.github.com
     password $GITHUB_TOKEN
 EOF
 
-if [[ "$ticket" != "" && ( "$body" != "$new_body" || "$title" != "$new_title" ) ]]; then
+if [[ "$story" != "" && ( "$body" != "$new_body" || "$title" != "$new_title" ) ]]; then
     "$OK" update_pull_request "$GITHUB_REPOSITORY" "$number" "body='$new_body'" "title='$new_title'"
+fi
+
+# If we have no story add a comment to create one
+if [[ -z "$story" ]] && [[ -n "$CREATE_STORY_URL" ]]; then
+    "$OK" add_comment "$GITHUB_REPOSITORY" "$number" \
+        "We could not find a CH story in this PR.  Please find or [create a story]<$CREATE_STORY_URL> and add it to the PR title or description."
 fi

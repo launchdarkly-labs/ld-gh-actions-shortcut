eafb78ed4da0c5c6d318ce67d35788eaab484f95
Andrew Shannon Brown <ashanbrown@users.noreply.github.com>
2021-04-26 14:46:08 -0700

    Add SKIP_COMMENT option to skip adding comment
    The clubhouse integration now adds comments so we don't need to

--- set-ch-link/entrypoint.sh
+++ set-ch-link/entrypoint.sh
@@ -89,7 +89,7 @@ if [[ "$story" != "" && ( "$body" != "$new_body" || "$title" != "$new_title" ) ]
         args+=("body='$new_body'")
     fi
     "$OK" update_pull_request "$GITHUB_REPOSITORY" "$number" "${args[@]}"
-    if [[ "$COMMENT_ONLY" == "1" && "$action" == "opened" ]]; then
+    if [[ "$COMMENT_ONLY" == "1" && "$SKIP_COMMENT" != "1" && "$action" == "opened" ]]; then
         "$OK" add_comment "$GITHUB_REPOSITORY" "$number" "CH link is $link_url."
     fi
 fi

96250dcde7bf9f88acc412b4943750d3515ff2c9
Case Delst <casedelst@gmail.com>
2022-11-18 15:49:11 -0800

    Add sass

--- set-link/entrypoint.sh
+++ set-link/entrypoint.sh
@@ -17,6 +17,7 @@ body=${body//$'\r'/} # Remove /r, which confuses jq in ok.sh
 title=$(jq -r .pull_request.title "$GITHUB_EVENT_PATH")
 title=${title//$'\r'/} # Remove /r, which confuses jq in ok.sh
 branch=$(jq -r .pull_request.head.ref "$GITHUB_EVENT_PATH")
+user=$(jq -r .pull_request.user.login "$GITHUB_EVENT_PATH")
 
 echo "Current PR title is '${title}'"
 echo "Github branch is '$branch'"
@@ -72,10 +73,14 @@ if [[ "$formatted_title" != " " && "$formatted_title" != "" ]]; then
   new_title="${formatted_title}"
 fi
 
-new_title=${new_title/"[sc-$story]"/""}
+# Remove the story from anywhere in the name
+story_removed_title=${new_title/"[sc-$story]"/""}
+before_story_removed="${new_title}"
+
+echo "New title with removed ticket is '${new_title}'"
 
 # Add the story number to the PR title if it isn't already there
-if [[ "$new_title" != *"[sc-$story]"* ]]; then
+if [[ "$story_removed_title" != *"[sc-$story]"* ]]; then
     new_title="[sc-${story}] ${new_title^}"
 fi
 
@@ -96,6 +101,10 @@ if [[ "$story" != "" && ( "$body" != "$new_body" || "$title" != "$new_title" ) ]
     "$OK" update_pull_request "$GITHUB_REPOSITORY" "$number" "${args[@]}"
     if [[ "$COMMENT_ONLY" == "1" && "$SKIP_LINK" != "1" && "$action" == "opened" ]]; then
         "$OK" add_comment "$GITHUB_REPOSITORY" "$number" "Shortcut link is $link_url."
+    fi
+
+    if [[ "${story_removed_title}" != "${before_story_removed}" ]]; then
+            "$OK" add_comment "$GITHUB_REPOSITORY" "$number" "Ahem, @${user}.  I know it's fun to add the shortcut ticket number to the PR name manually, but I'm here to help (also, it's one of the only reasons I exist).  Also, it's less fun for me if you do it :("
     fi
 fi
 

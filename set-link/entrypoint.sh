eedbebf495b5c5e26c348751ce74f057f82a7d9f
Hosh Sadiq <hoshsadiq@users.noreply.github.com>
2023-02-16 15:37:36 +0000

    Prevent posting comment when shortcut bot is enabled

--- set-link/entrypoint.sh
+++ set-link/entrypoint.sh
@@ -1,49 +1,56 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+[[ -z ${GITHUB_TOKEN:-} ]] && printf >&2 "error: GITHUB_TOKEN missing\n" && exit 1
+
 OK=ok.sh
+AUTOLINK_PREFIX="${AUTOLINK_PREFIX:-}"
+STORY_BASE_URL="${STORY_BASE_URL:-}"
+STORY_LINK_TEXT="${STORY_LINK_TEXT:-}"
+COMMENT_ONLY="${COMMENT_ONLY:-}"
+SKIP_LINK="${SKIP_LINK:-}"
 
 echo "Received event:"
-cat "$GITHUB_EVENT_PATH"
+#cat "$GITHUB_EVENT_PATH"
 echo
 
 number=$(jq -r .number "$GITHUB_EVENT_PATH")
 action=$(jq -r .action "$GITHUB_EVENT_PATH")
-if  [[ "$action" != "edited" ]] && [[ "$action" != "opened" ]] && [[ "$action" != "reopened" ]]; then
-    exit 0
+if [[ "$action" != "edited" ]] && [[ "$action" != "opened" ]] && [[ "$action" != "reopened" ]]; then
+  exit 0
 fi
 
-body=$(jq -r .pull_request.body "$GITHUB_EVENT_PATH")
+body="$(jq -r .pull_request.body "$GITHUB_EVENT_PATH")"
 body=${body//$'\r'/} # Remove /r, which confuses jq in ok.sh
-title=$(jq -r .pull_request.title "$GITHUB_EVENT_PATH")
+title="$(jq -r .pull_request.title "$GITHUB_EVENT_PATH")"
 title=${title//$'\r'/} # Remove /r, which confuses jq in ok.sh
-branch=$(jq -r .pull_request.head.ref "$GITHUB_EVENT_PATH")
-user=$(jq -r .pull_request.user.login "$GITHUB_EVENT_PATH")
+branch="$(jq -r .pull_request.head.ref "$GITHUB_EVENT_PATH")"
+user="$(jq -r .pull_request.user.login "$GITHUB_EVENT_PATH")"
 
 echo "Current PR title is '${title}'"
 echo "Github branch is '$branch'"
 
-pattern='.*\bsc-\{0,1\}\([[:digit:]]\+\)\b.*'
-story_from_title=$(expr "$title" : "$pattern")
-story_from_branch=$(expr "$branch" : "$pattern")
+stories=()
 
-link_pattern='.*\story/\([[:digit:]]\+\)\b.*'
-story_from_body=$(expr "$body" : "$pattern")
-story_from_body_link=$(expr "$body" : "$link_pattern")
+pattern=".*\bsc-?([[:digit:]]+)\b.*"
+[[ $title =~ $pattern ]] && stories+=("${BASH_REMATCH[1]}")
+[[ $branch =~ $pattern ]] && stories+=("${BASH_REMATCH[1]}")
+
+link_pattern=".*story/([[:digit:]]+)\b.*"
+[[ $body =~ $pattern ]] && stories+=("${BASH_REMATCH[1]}")
+[[ $body =~ $link_pattern ]] && stories+=("${BASH_REMATCH[1]}")
 
 if [[ -n "$AUTOLINK_PREFIX" ]]; then
-  autolink_pattern="${AUTOLINK_PREFIX}\([[:digit:]]\+\)\b.*"
-  story_from_autolink=$(expr "$body" : "$autolink_pattern")
+  autolink_pattern="${AUTOLINK_PREFIX}([[:digit:]]+)\b.*"
+  [[ $body =~ $autolink_pattern ]] && stories+=("${BASH_REMATCH[1]}")
 fi
 
-# Check title first for the CH story
-# shellcheck disable=SC2206
-stories=($story_from_title $story_from_branch $story_from_body $story_from_autolink $story_from_body_link)
 story="${stories[0]}"
-
 if [[ -z "$story" ]]; then
-    echo "Could not find Shortcut story"
+  echo "Could not find Shortcut story"
 else
-    echo "Found Shortcut story number '${story}'"
+  echo "Found Shortcut story number '${story}'"
 fi
 
 # If we have an autolink prefix to use, we use that instead of a link
@@ -55,20 +62,19 @@ else
 fi
 
 if [[ -n "$story" ]]; then
-    new_body=${body/$STORY_LINK_TEXT/$link_url} # replace the story link text
+  new_body=${body/$STORY_LINK_TEXT/$link_url} # replace the story link text
 fi
 
 # If we still don't have the url in the body, tack it on the beginning
-if [[ "$new_body" != *"$link_url"* ]] && [[ -n "$story" ]] ; then
-    new_body="$link_url\n$new_body"
+if [[ "$new_body" != *"$link_url"* && -n "$story" ]]; then
+  new_body="$link_url\n$new_body"
 fi
 
 new_title="${title}"
 
-formatted_title=`echo "${new_title}" | cut -d "/" -f 3`
+formatted_title="$(cut <<<"${new_title}" -d "/" -f3)"
 
 echo "Formatted title is '${formatted_title}'"
-
 if [[ "$formatted_title" != " " && "$formatted_title" != "" ]]; then
   new_title="${formatted_title}"
 fi
@@ -84,35 +90,34 @@ echo "New title with removed ticket is '${new_title}'"
 
 # Add the story number to the PR title if it isn't already there
 if [[ "$story_removed_title" != *"[sc-$story]"* ]]; then
-    new_title="[sc-${story}] ${story_removed_title^}"
+  new_title="[sc-${story}] ${story_removed_title^}"
 fi
 
 echo "Final new title is '${new_title}'"
 
-cat > ~/.netrc <<-EOF
-machine api.github.com
-    login $GITHUB_ACTOR
-    password $GITHUB_TOKEN
-EOF
+"$OK" -j list_issue_comments "$GITHUB_REPOSITORY" "$number" | jq -e '. | map(select(.user?.login? == "shortcut-integration[bot]")) | .[0]' >/dev/null
+SKIP_COMMENT="$?"
 
-if [[ "$story" != "" && ( "$body" != "$new_body" || "$title" != "$new_title" ) ]]; then
-    args=("title='$new_title'")
-    if [[ "$COMMENT_ONLY" != "1" && "$SKIP_LINK" != "1" ]]; then
-        args+=("body='$new_body'")
-    fi
-    "$OK" update_pull_request "$GITHUB_REPOSITORY" "$number" "${args[@]}"
-    if [[ "$COMMENT_ONLY" == "1" && "$SKIP_LINK" != "1" && "$action" == "opened" ]]; then
-        "$OK" add_comment "$GITHUB_REPOSITORY" "$number" "Shortcut link is $link_url."
+if [[ "$story" != "" ]] && { [[ "$body" != "$new_body" || "$title" != "$new_title" ]]; }; then
+  args=()
+  [[ "$title" != "$new_title" ]] && args+=("title='$new_title'")
+  if [[ "$body" != "$new_body" && "$COMMENT_ONLY" != "1" && "$SKIP_LINK" != "1" ]]; then
+    args+=("body='$new_body'")
+  fi
+  "$OK" update_pull_request "$GITHUB_REPOSITORY" "$number" "${args[@]}"
 
-        if [[ "${story_removed_title}" != "${before_story_removed}" ]]; then
-                    "$OK" add_comment "$GITHUB_REPOSITORY" "$number" "Ahem, @${user}.  I know it's fun to add the shortcut ticket number to the PR name manually, but I'm here to help (also, it's one of the only reasons I exist).  It's less fun for me if you do it :("
-        fi
+  if [[ "$COMMENT_ONLY" == "1" && "$SKIP_LINK" != "1" && "$SKIP_COMMENT" == "0" && "$action" == "opened" ]]; then
+    echo "$OK" add_comment "$GITHUB_REPOSITORY" "$number" "Shortcut link is $link_url."
+
+    if [[ "${story_removed_title}" != "${before_story_removed}" ]]; then
+      echo "$OK" add_comment "$GITHUB_REPOSITORY" "$number" "Ahem, @${user}.  I know it's fun to add the shortcut ticket number to the PR name manually, but I'm here to help (also, it's one of the only reasons I exist).  It's less fun for me if you do it :("
     fi
+  fi
 fi
 
 # If we have no story add a comment to create one
 draft=$(jq -r .pull_request.draft "$GITHUB_EVENT_PATH")
-if { [[ "$action" = "opened" ]] || [[ "$action" = "reopened" ]]; } && [[ -z "$story" ]] && [[ "$draft" != "true" ]] && [[ -n "$CREATE_STORY_URL" ]]; then
-    "$OK" add_comment "$GITHUB_REPOSITORY" "$number" \
-        "We could not find a Shortcut story in this PR.  Please find or [create a story]($CREATE_STORY_URL) and add it to the PR title or description."
+if [[ "$SKIP_COMMENT" == "0" ]] && { [[ "$action" = "opened" || "$action" = "reopened" ]]; } && [[ -z "$story" && "$draft" != "true" && -n "$CREATE_STORY_URL" ]]; then
+  echo "$OK" add_comment "$GITHUB_REPOSITORY" "$number" \
+    "We could not find a Shortcut story in this PR.  Please find or [create a story]($CREATE_STORY_URL) and add it to the PR title or description."
 fi
